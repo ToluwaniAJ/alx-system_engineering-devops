940090ce3262571c7d830b5da937a07c34a3b923
Toluwani Ajibare <toluwaniajibare@gmail.com>
2023-04-25 05:25:13 +0100

    fixed documentation errors

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -4,9 +4,10 @@ import csv
 import requests
 import sys
 
-if len(sys.argv) != 2:
-    print("Please provide an employee ID as a parameter.")
-    exit()
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Please provide an employee ID as a parameter.")
+        exit()
 
 employee_id = sys.argv[1]
 url = "https://jsonplaceholder.typicode.com/"
@@ -19,16 +20,20 @@ try:
     username = user.get("username")
 
     # Fetch todos for the employee
-    todos_response = requests.get(f"{url}todos", params={"userId": employee_id})
+    todos_response = requests.get(
+        f"{url}todos", params={
+            "userId": employee_id})
     todos_response.raise_for_status()
     todos = todos_response.json()
 
     # Write todos to CSV file
     with open(f"{employee_id}.csv", "w", newline="") as csvfile:
         writer = csv.writer(csvfile)
-        writer.writerow(["USER_ID", "USERNAME", "TASK_COMPLETED_STATUS", "TASK_TITLE"])
+        writer.writerow(
+            ["USER_ID", "USERNAME", "TASK_COMPLETED_STATUS", "TASK_TITLE"])
         for todo in todos:
-            writer.writerow([employee_id, username, todo.get("completed"), todo.get("title")])
+            writer.writerow([employee_id, username, todo.get(
+                "completed"), todo.get("title")])
 
     print(f"Todo list for employee {username} exported to {employee_id}.csv")
 
